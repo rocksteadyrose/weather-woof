af2eea1c8930bb3677ffc7fe0032a8104fdc2204
Leslie Rose <leslierose@Leslies-MBP-2.attlocal.net>
2019-07-14 20:40:42 -0500

    changes to functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -36,6 +36,8 @@ module.exports = function (app) {
     // This is the user data coming from the user. req.body is available since we're using the body-parser middleware
     // var zip = dogData.zip;
     var dogInfo = req.body;
+    var points = 100;
+
     console.log(dogInfo)
     // var dogsTested = breedArray;
 
@@ -44,118 +46,140 @@ module.exports = function (app) {
             // console.log(response.current.feelslike_f)
 
             if (dogInfo.feelsLike > 80 && dogInfo.feelsLike < 90) {
-              dogInfo.points += -5;
-                console.log(dogInfo.points)
+              points += -5;
+                console.log(points)
             }
 
             if (dogInfo.feelsLike > 90 && dogInfo.feelsLike < 95) {
-              dogInfo.points += -10;
-                console.log(dogInfo.points)
+              points += -10;
+                console.log(points)
             }
 
             if (dogInfo.feelsLike > 95 && dogInfo.feelsLike < 100) {
-              dogInfo.points += -15;
-                console.log(dogInfo.points)
+              points += -15;
+                console.log(points)
             }
 
             if (dogInfo.feelsLike > 100) {
-              dogInfo.points += -50;
-                console.log(dogInfo.points)
-            }
-
-            if (dogInfo.feelsLike < 50 && dogInfo.feelsLike > 40) {
-              dogInfo.points += -20;
-                console.log(dogInfo.points)
+              points += -50;
+                console.log(points)
             }
 
             if (dogInfo.feelsLike < 40 && dogInfo.feelsLike > 20) {
-              dogInfo.points += -30;
-                console.log(dogInfo.points)
+              points += -25;
+                console.log(points)
             }
 
             if (dogInfo.feelsLike < 10 && dogInfo.feelsLike > 20) {
-              dogInfo.points += -40;
-                console.log(dogInfo.points)
+              points += -10;
+                console.log(points)
             }
 
             if (dogInfo.feelsLike < 10) {
-              dogInfo.points += -50;
-                console.log(dogInfo.points)
+              points += -50;
+                console.log(points)
             }
 
             if (dogInfo.feelsLike < 50 && dogInfo.condition === "Mist" || dogInfo.condition === "Patchy rain possible" || dogInfo.condition === "Freezing fog" || dogInfo.condition === "Patchy light drizzle" || dogInfo.condition === "Light drizzle" || dogInfo.condition === "Patchy light rain" || dogInfo.condition === "Light rain" || dogInfo.condition === "Moderate rain at times" || dogInfo.condition === "Light rain shower") {
-              dogInfo.points += -30;
-                console.log(dogInfo.points)
+              points += -30;
+                console.log(points)
             }
 
             if (dogInfo.feelsLike < 50 && dogInfo.condition === "Patchy sleet possible" || dogInfo.condition === "Patchy freezing drizzle possible" || dogInfo.condition === "Thundery outbreaks possible" || dogInfo.condition === "Blowing snow" || dogInfo.condition === "Blizzard" || dogInfo.condition === "Freezing drizzle" || dogInfo.condition === "Heavy freezing drizzle" || dogInfo.condition === "Moderate rain" || dogInfo.condition === "Heavy rain at times" || dogInfo.condition === "Heavy rain" || dogInfo.condition === "Light freezing rain" || dogInfo.condition === "Moderate or heavy freezing rain" || dogInfo.condition === "Light sleet" || dogInfo.condition === "Moderate or heavy sleet" || dogInfo.condition === "Patchy light snow" || dogInfo.condition === "Light snow" || dogInfo.condition === "Patchy moderate snow" || dogInfo.condition === "Moderate snow" || dogInfo.condition === "Patchy heavy snow" || dogInfo.condition === "Heavy snow" || dogInfo.condition === "Ice pellets" || dogInfo.condition === "Light rain shower" || dogInfo.condition === "Moderate or heavy rain shower" || dogInfo.condition === "Torrential rain shower" || dogInfo.condition === "Light sleet showers" || dogInfo.condition === "Moderate or heavy sleet showers" || dogInfo.condition === "Light snow showers" || dogInfo.condition === "Moderate or heavy snow showers" || dogInfo.condition === "Light showers of ice pellets" || dogInfo.condition === "Light showers of ice pellets" || dogInfo.condition === "Moderate or heavy showers of ice pellets" || dogInfo.condition === "Patchy light rain with thunder" || dogInfo.condition === "Moderate or heavy rain with thunder" || dogInfo.condition === "Patchy light snow with thunder" || dogInfo.condition === "Moderate or heavy snow with thunder") {
-              dogInfo.points += -50;
-                console.log(dogInfo.points)
+              points += -50;
+                console.log(points)
             }
 
             // for (i = 0; i < shortNose.length; i++) {
 
             //     if (dogInfo.shortNose.indexOf(specificBreed[i]) > -1 && dogInfo.feelsLike > 80) {
-            //       dogInfo.points += -10;
-            //         console.log(dogInfo.points)
+            //       points += -10;
+            //         console.log(points)
             //     }
             // }
 
             // for (i = 0; i < thickFur.length; i++) {
 
             //     if (dogInfo.thickFur.indexOf(specificBreed[i]) > -1 && feelsdogInfo.feelsLikeLike > 80) {
-            //       dogInfo.points += -10;
-            //         console.log(dogInfo.points)
+            //       points += -10;
+            //         console.log(points)
             //     }
             // }
 
-            if (dogInfo.furColor === "Black" && dogInfo.feelsLike > 80 || dogInfo.furColor === "Multi - Mostly Dark Colored" && dogInfo.feelsLike > 75) {
-              dogInfo.points += -10;
-                console.log(dogInfo.points)
+            if (dogInfo.furColor === "Black") {
+              points += -20;
+                console.log(points)
             }
+
+           if (dogInfo.furColor === "Multi - Mostly Dark Colored") {
+            points += -15;
+              console.log(points)
+          }
 
             if (dogInfo.health === "Poor" && dogInfo.feelsLike > 80 || dogInfo.health === "Very Poor" && dogInfo.feelsLike > 75 || dogInfo.health === "Poor" && dogInfo.feelsLike < 35 || dogInfo.health === "Very Poor" && dogInfo.feelsLike < 40) {
-              dogInfo.points += -20;
-                console.log(dogInfo.points)
+              points += -30;
+                console.log(points)
             }
 
-            if (dogInfo.Question === "Yes") {
-              dogInfo.points += -15;
-                console.log(dogInfo.points)
+            if (dogInfo.weightQuestion === "Yes") {
+              points += -15;
+                console.log(points)
             }
 
             //Puppy
             if (dogInfo.age === "Under 1") {
-              dogInfo.points += -10;
-                console.log(dogInfo.points)
+              points += -10;
+                console.log(points)
             }
 
             //Toy & Small Dog
             if (dogInfo.weight < 15 && dogInfo.age > 10) {
-              dogInfo.points += -10;
-                console.log(dogInfo.points)
+              points += -30;
+                console.log(points)
             }
 
             //Medium Dog
             if (dogInfo.weight < 50 && dogInfo.weight > 15 && dogInfo.age > 8) {
-              dogInfo.points += -10;
-                console.log(dogInfo.points)
+              points += -30;
+                console.log(points)
             }
 
             //Large Dog
             if (dogInfo.weight < 80 && dogInfo.weight > 50 && dogInfo.age > 7) {
-              dogInfo.points += -10;
-                console.log(dogInfo.points)
+              points += -30;
+                console.log(points)
             }
 
             //Giant Dog
             if (dogInfo.weight > 80 && dogInfo.age > 5) {
-              dogInfo.points += -10;
-                console.log(dogInfo.points)
+              points += -30;
+                console.log(points)
                 //Source: The Living Well Guide for Senior Dogs, Diane Morgan, Wayne Hunthausen DVM
             }
 
-            var shouldntwalk = "testttt"
+            var shouldntwalk;
+
+            if (points > 80) {
+              shouldntwalk = "Your dog is totally fine to walk"
+            }
+            
+            if (points < 80 || points > 60) {
+              shouldntwalk = "Your dog should be okay to walk"
+            } 
+            
+            if (points < 60 || points > 40) {
+              shouldntwalk = "Please be mindful if walking your dog"
+            } 
+            
+            if (points < 40 || points > 20) {
+              shouldntwalk = "Please take extreme caution if walking your dog"
+            }
+            
+            if (points < 20) {
+              shouldntwalk = "You should absolutely not walk your dog"
+            }
+
+            console.log(points)
 
             //send the match to the front-end/json for the module
             res.json({ shouldntwalk});
